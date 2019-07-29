8da296e888ae1255ca8986e4ebbfcc3116bb1658
nzws <git@nzws.me>
2019-07-30 08:44:01 +0900

    Add imagemin

---
+++ scripts/imagemin.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+
+(async () => {
+  const files = await imagemin([path.join(__dirname, '../image_draft/*.{jpg,png}')], {
+    destination: path.join(__dirname, '../source/images'),
+    plugins: [
+      imageminJpegtran(),
+      imageminPngquant()
+    ]
+  });
+
+  files.forEach(file => {
+    fs.unlinkSync(file.sourcePath);
+  });
+})();
